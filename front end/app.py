fa48c930762e15528f83a119ce7cf092a0119dba
freddiehw1 <bn21737@bristol.ac.uk>
2024-02-25 21:53:11 +0000

    lets goo

--- front end/app.py
+++ front end/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
-from flask import Flask, request, jsonify
-#from NN import predict
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from inference import load_model, predict_image
+import os
+from inference import load_model, predict_image
+
 app = Flask(__name__)
+
+# Load the model, class labels, mean, and std
+model_load_path = 'pretrained_mobilenetv2.pth'
+model, class_labels, mean, std = load_model(model_load_path)
+
 answers_dict = {}
+UPLOAD_FOLDER = 'uploads'  # Directory to store uploaded files
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Create the uploads directory if it doesn't exist
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/skin_cancer')
 def skin_cancer():
@@ -20,10 +32,46 @@ def lung_cancer():
 def submit():
     global answers_dict
     data = request.json
-    # Process the data (e.g., store it in a database, perform analysis)
-    answers_dict.update(data)  # Update the answers dictionary with the received data
-    print(answers_dict)  # Print the updated dictionary (optional)
-   # label = predict(answers_dict)
+    answers_dict.update(data)
+    print(answers_dict)
     return jsonify({'message': 'Data received successfully'})
+
+@app.route('/classify', methods=['POST'])
+
+def upload_image():
+    if 'fileInput' not in request.files:
+        return jsonify({'error': 'No file part'})
+    
+    file = request.files['fileInput']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    
+    # Save the uploaded file to the uploads directory
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'temp.jpg'))
+
+    #Classification
+    model_load_path = 'pretrained_mobilenetv2.pth'
+
+    # Load the model, class labels, mean, and std
+    model, class_labels, mean, std = load_model(model_load_path)
+
+    # Path to the input image
+    input_image_path = '/Users/freddiehurdwood/Desktop/Uni Work/ForCancer/uploads/temp.jpg'
+
+    # Perform inference
+    predicted_class, probabilities = predict_image(input_image_path, model, class_labels, mean, std)
+    print(predicted_class, probabilities)
+    os.remove('/Users/freddiehurdwood/Desktop/Uni Work/ForCancer/uploads/temp.jpg')
+#classifcation ends
+    
+    return jsonify({'message': 'Image saved successfully'})
+
+
+@app.route('/result')
+def result():
+    predicted_class = request.args.get('predicted_class')
+    probabilities = request.args.get('probabilities')
+    return render_template('result.html', predicted_class=predicted_class, probabilities=probabilities)
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ front end/inference.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+
+def load_model(model_load_path):
+    # Load the model and metadata
+    checkpoint = torch.load(model_load_path)
+    num_classes = checkpoint['num_classes']
+    class_labels = checkpoint['class_labels']
+    mean = checkpoint['mean']
+    std = checkpoint['std']
+
+    # Load the model architecture
+    model = torchvision.models.mobilenet_v2(pretrained=False)
+    model.classifier[1] = nn.Linear(model.last_channel, num_classes)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+
+    return model, class_labels, mean, std
+
+def preprocess_image(image_path, mean, std):
+    # Preprocess the image
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+    image = Image.open(image_path)
+    image = transform(image).unsqueeze(0)  # Add batch dimension
+    return image
+
+def predict_image(image_path, model, class_labels, mean, std):
+    # Preprocess the image
+    input_image = preprocess_image(image_path, mean, std)
+    
+    # Perform inference
+    with torch.no_grad():
+        # Forward pass
+        logits = model(input_image)
+        # Get predicted class probabilities
+        probabilities = torch.softmax(logits, dim=1)[0]
+        # Get the predicted class index
+        predicted_class_index = torch.argmax(probabilities).item()
+    
+    # Get the predicted class label
+    predicted_class_label = class_labels[predicted_class_index]
+    top_prob = probabilities[predicted_class_index]
+    top_prob = top_prob.item()
+    rounded_prob = round(top_prob * 100, 2)
+    
+    return predicted_class_label, rounded_prob
